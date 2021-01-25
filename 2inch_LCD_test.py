49b1a1a010f519a3d1ba56f458803f0996088ff4
mrilboom <1174852972@qq.com>
2021-01-25 09:57:26 +0800

    测试显示图片demo，最后清屏，看看图像残留bug是否还在

--- 2inch_LCD_test.py
+++ 2inch_LCD_test.py
@@ -27,54 +27,14 @@ try:
     # Clear display.
     disp.clear()
 
-    # Create blank image for drawing.
-    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
-    draw = ImageDraw.Draw(image1)
-
-    logging.info("draw point")
-    #画基本的图线 ，微雪的相关logo
-    draw.rectangle((5,10,6,11), fill = "BLACK")
-    draw.rectangle((5,25,7,27), fill = "BLACK")
-    draw.rectangle((5,40,8,43), fill = "BLACK")
-    draw.rectangle((5,55,9,59), fill = "BLACK")
-
-    logging.info("draw line")
-    draw.line([(20, 10),(70, 60)], fill = "RED",width = 1)
-    draw.line([(70, 10),(20, 60)], fill = "RED",width = 1)
-    draw.line([(170,15),(170,55)], fill = "RED",width = 1)
-    draw.line([(150,35),(190,35)], fill = "RED",width = 1)
-
-    logging.info("draw rectangle")
-    draw.rectangle([(20,10),(70,60)],fill = "WHITE",outline="BLUE")
-    draw.rectangle([(85,10),(130,60)],fill = "BLUE")
-
-    logging.info("draw circle")
-    draw.arc((150,15,190,55),0, 360, fill =(0,255,0))
-    draw.ellipse((150,65,190,105), fill = (0,255,0))
-
-    logging.info("draw text")
-    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
-    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
-    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
-
-    draw.rectangle([(0,65),(140,100)],fill = "WHITE")
-    draw.text((5, 68), 'Hello world', fill = "BLACK",font=Font1)
-    draw.rectangle([(0,115),(190,160)],fill = "RED")
-    draw.text((5, 118), 'WaveShare', fill = "WHITE",font=Font2)
-    draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
-    text= u"微雪电子"
-    draw.text((5, 200),text, fill = "BLUE",font=Font3)
-
-    image1=image1.rotate(180)
-    #show image,PIL object disp
-    disp.ShowImage(image1)
-    time.sleep(3)
     logging.info("show image")
-    image = Image.open('../pic/LCD_2inch.jpg')	
-    image = image.rotate(180)
+    image = Image.open('../pic/LCD_2inch.jpg')
+    #resize 平滑滤波
+    image = image.resize((240, 320),Image.ANTIALIAS)
     disp.ShowImage(image)
     time.sleep(3)
-    #此处图像显示之后模块无法正常退出，debug输出没问题，接线错误？
+    logging.info("clear...")
+    disp.clear()
     disp.module_exit()
     logging.info("quit:")
 except IOError as e:
