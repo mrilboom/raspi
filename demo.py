9a6d7a9319e5337aa31386e6f83eb8d12873815c
mrilboom <1174852972@qq.com>
2021-01-25 10:43:20 +0800

    stream 读写测试

--- demo.py
+++ demo.py
@@ -13,17 +13,18 @@ bus = 0
 device = 0 
 logging.basicConfig(level=logging.DEBUG)
 # 创建流
-stream = io.BytesIO()
+stream = open('stream.jpg', 'wb')
 with picamera.PiCamera() as camera:
+    #设置像素和帧率
     camera.resolution = (240, 320)
+    camera.framerate = 30
     camera.start_preview()
     #摄像头预热
     time.sleep(2)
     camera.capture(stream, format='jpeg')
 # 将指针指向流的开始
-stream.seek(0)
-image = Image.open(stream)
 
+#调用xxx.close来关闭流
 try:
     # init
     disp = LCD_2inch.LCD_2inch()
@@ -32,11 +33,14 @@ try:
 
     logging.info("show image")
     while True:
+        image = Image.open(stream)
         disp.ShowImage(image)
 except IOError as e:
+    stream.close()
     disp.module_exit()
     logging.info(e)
 except KeyboardInterrupt:
+    stream.close()
     disp.module_exit()
     logging.info("quit: KeyboardInterrupt")
     exit()
