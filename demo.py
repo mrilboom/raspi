d5ca834e47044e9a84335f65e04dc8b681c31001
mrilboom <1174852972@qq.com>
2021-01-25 11:24:50 +0800

    捕获流

--- demo.py
+++ demo.py
@@ -12,35 +12,30 @@ BL = 18
 bus = 0 
 device = 0 
 logging.basicConfig(level=logging.DEBUG)
-# 创建流
-stream = open('stream.jpg', 'wb')
-with picamera.PiCamera() as camera:
-    #设置像素和帧率
-    camera.resolution = (240, 320)
-    camera.framerate = 30
-    camera.start_preview()
-    #摄像头预热
-    time.sleep(2)
-    camera.capture(stream, format='jpeg')
-# 将指针指向流的开始
 
-#调用xxx.close来关闭流
 try:
-    # init
+     # init
     disp = LCD_2inch.LCD_2inch()
     disp.Init()
     disp.clear()
+    #创建流
 
-    logging.info("show image")
-    while True:
-        image = Image.open(stream)
-        disp.ShowImage(image)
+    stream = io.BytesIO()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (240, 320)
+        camera.framerate = 15
+        camera.start_preview()
+        # 摄像头预热
+        time.sleep(2)
+        camera.capture(stream, 'jpeg')
+        logging.info("show image")
+        while True:
+            image = Image.open(stream)
+            disp.ShowImage(image)
 except IOError as e:
-    stream.close()
     disp.module_exit()
     logging.info(e)
 except KeyboardInterrupt:
-    stream.close()
     disp.module_exit()
     logging.info("quit: KeyboardInterrupt")
     exit()
