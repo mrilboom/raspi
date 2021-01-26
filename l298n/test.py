105e934887b98d76e6a4a41a1fef15948e2d279f
mrilboom <1174852972@qq.com>
2021-01-26 06:05:45 +0000

    进一步改进，gpio.cleanup问题修复，加入键盘控制

--- l298n/test.py
+++ l298n/test.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
+import keyboard
 
 #编码方式
 GPIO.setmode(GPIO.BOARD)
@@ -22,14 +22,13 @@ def init()
 '''
 L298N模块IN1、IN2和IN1、IN2分别对应两个马达，输出IN1>IN2为前进，IN1<IN2为后退
 pwm调速暂未启用
+树莓派和l298n必须公地，否则无法启动！
 '''
 def forward()
     GPIO.output(IN1,GPIO.HIGH)
     GPIO.output(IN2,GPIO.LOW)
     GPIO.output(IN3,GPIO.HIGH)
     GPIO.output(IN4,GPIO.LOW)
-    time.sleep(2)
-    GPIO.cleanup()
 
 
 def backward()
@@ -37,8 +36,6 @@ def backward()
     GPIO.output(IN2,GPIO.HIGH)
     GPIO.output(IN3,GPIO.LOW)
     GPIO.output(IN4,GPIO.HIGH)
-    time.sleep(2)
-    GPIO.cleanup()
 
 
 def left():
@@ -46,19 +43,31 @@ def left():
     GPIO.output(IN2,False)
     GPIO.output(IN3,GPIO.HIGH)
     GPIO.output(IN4,GPIO.LOW)
-    time.sleep(2)
-    GPIO.cleanup()
- 
+
 
 def right():
     GPIO.output(IN1,GPIO.HIGH)
     GPIO.output(IN2,GPIO.LOW)
     GPIO.output(IN3,False)
     GPIO.output(IN4,False)
-    time.sleep(2)
-    GPIO.cleanup()
 
-init()
-print("moving")
-forward()
-print("success")
+try:
+    init()
+    while True:
+        if keyboard.is_pressed('w'):
+            print('Forward')
+            forward()
+        elif keyboard.is_pressed('s'):
+            print('Backward')
+            backward()
+        elif keyboard.is_pressed('a'):
+            print('Left')
+            left()
+        elif keyboard.is_pressed('d'):
+            print('Right')
+            right()
+        elif keyboard.is_pressed('q'):
+            print('Quit!')
+except:
+    GPIO.cleanup()
+    print("error...")
